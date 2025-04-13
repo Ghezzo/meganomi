fa2e83447eb37b5ba3f121f374fe00fb7eecb9e4
Ghezzo <1338300+Ghezzo@users.noreply.github.com>
2025-04-13 15:48:19 +0200

    Update meganomi.user.js
    add version to settings panel

--- meganomi.user.js
+++ meganomi.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mega Nomi Script beta
 // @namespace    https://gzo.sh
-// @version      0.8.1
+// @version      0.8.2
 // @description  Everything in one :)
 // @author       Ghezzo
 // @match        https://beta.nomi.ai/nomis*
@@ -39,6 +39,7 @@
     settingsPanel.style.color = 'white'; // add padding to the panel
     settingsPanel.style.padding = '10px'; // add padding to the panel
     settingsPanel.innerHTML = `
+    <span>Version 0.8.2</span>
     <h2>Settings</h2>
     <label>
         Asterisk Style:<br>
