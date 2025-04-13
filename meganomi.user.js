3f647fe9cd37490d98cd722257029aae41e619c3
Ghezzo <1338300+Ghezzo@users.noreply.github.com>
2025-04-13 15:24:57 +0200

    bump version

--- meganomi.user.js
+++ meganomi.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mega Nomi Script beta
 // @namespace    https://gzo.sh
-// @version      0.7
+// @version      0.8
 // @description  Everything in one :)
 // @author       Ghezzo
 // @match        https://beta.nomi.ai/nomis*
